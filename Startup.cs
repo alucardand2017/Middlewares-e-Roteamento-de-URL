6b99ba7ff70c21721165a61141a95bb772cc998c
ANDERSON SILVA <alucardand2017@gmail.com>
2024-01-25 23:10:32 -0300

    Passando Rotas variáveis em Endpoints com passagem de parametro padrão caso haja omissao

--- Startup.cs
+++ Startup.cs
@@ -19,16 +19,32 @@ namespace AULA13ROTEAMENTOURLS
             //app.UseMiddleware<MiddlewareConsultaPopulacao>();
             //app.UseMiddleware<MiddlewareConsultaCep>();
 
+
+
             app.UseRouting();
             app.UseEndpoints(endpoints => {
+
+                endpoints.MapGet("arq/{arquivo}.{ext}" , async context =>
+                {
+                    context.Response.ContentType = "text/plain; charset=uft-8";
+                    await context.Response.WriteAsync("Requisicao foi roteada. \n");
+                    foreach(var item in context.Request.RouteValues)
+                    {
+                        await context.Response.WriteAsync($"{item.Key}: {item.Value}\n");
+                    }
+                });
+
                 endpoints.MapGet("rota", async context => {
                     context.Response.ContentType = "text/plain; charset=utf-8";
                     await context.Response.WriteAsync("Requisição roteada");
                 });
-                endpoints.MapGet("pop/{local}", EndpointConsultaPop.Endpoint)
+
+                endpoints.MapGet("pop/{local=São%20Paulo-SP}", EndpointConsultaPop.Endpoint)
                     .WithMetadata(new RouteNameMetadata("consultapop"));
+
                 endpoints.MapGet("cep/{cep}", EndpointConsultaCep.Endpoint);
             });
+
             app.Use(async (context, next) => {
                 context.Response.ContentType = "text/plain; charset=utf-8";
                 await context.Response.WriteAsync("Middleware terminal alcançado. ");
